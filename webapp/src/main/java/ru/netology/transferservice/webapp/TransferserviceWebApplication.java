3007e209f10f6e51fe72ed98b266b2ae001f4d69
Alexander M Artamonov <art.alex.m@gmail.com>
2023-11-29 18:32:41 +0300

    Базовая структура проекта

---
+++ webapp/src/main/java/ru/netology/transferservice/webapp/TransferserviceWebApplication.java
@@ -0,0 +1,11 @@
+package ru.netology.transferservice.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferserviceWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TransferserviceWebApplication.class, args);
+    }
+}
